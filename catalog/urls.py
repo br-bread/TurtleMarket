2269a9edbcbb354ecb1030aabb99dbab06315ec0
ksusha <ktsykanova@yandex.ru>
2022-10-25 00:40:23 +0300

    add app_name's

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path, re_path
 
 from . import views
-
+app_name = 'catalog'
 urlpatterns = [
-    path('', views.item_list),
-    re_path('(?P<pk>^[1-9]\\d*)/', views.item_detail),
+    path('', views.item_list, name='item_list'),
+    re_path('(?P<pk>^[1-9]\\d*)/', views.item_detail, name='item_detail'),
 ]
