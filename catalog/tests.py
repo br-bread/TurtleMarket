3fd70a4edb8fd8a3ee6ee9b9eefd76f8e56795a1
ksusha <ktsykanova@yandex.ru>
2022-10-24 23:31:22 +0300

    update regex for catalog app + update tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -2,14 +2,20 @@ from django.test import Client, TestCase
 
 
 class StaticURLTests(TestCase):
-    def test_catalog_url(self):
-        response = Client().get('/catalog/')
-        self.assertEqual(response.status_code, 200, response.content)
-        response = Client().get('/catalog/1/')
-        self.assertEqual(response.status_code, 200)
-        response = Client().get('/catalog/1-sdj/')
-        self.assertNotEqual(response.status_code, 200)
-        response = Client().get('/catalog/ghg/')
-        self.assertNotEqual(response.status_code, 200)
-        response = Client().get('/catalog/-3/')
-        self.assertNotEqual(response.status_code, 200)
+    # Я не уверена, что это надо делать так, но я не поняла как по-другому
+    def test_positive_catalog_url(self):
+        tests = ['',  '1/',  '123/']
+
+        for i in range(len(tests)):
+            response = Client().get('/catalog/' + tests[i])
+            with self.subTest(f'{tests[i][:-1]} is a positive integer', i=i):
+                self.assertEqual(response.status_code, 200)
+
+    def test_negative_catalog_url(self):
+        tests = ['0', '-1', '0123', 'a', '1a', '1a2', 'a1',
+                 'one', '_-', '_1', '1_3', '123_']
+
+        for i in range(len(tests)):
+            response = Client().get('/catalog/' + tests[i] + '/')
+            with self.subTest(f'{tests[i][:]} is not a positive integer', i=i):
+                self.assertNotEqual(response.status_code, 200)
