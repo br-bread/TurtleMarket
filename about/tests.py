15fb8c2471c242b307023f54ef9b1ce4c3f7905e
ksusha <ktsykanova@yandex.ru>
2022-10-25 12:53:48 +0300

    update tests

--- about/tests.py
+++ about/tests.py
@@ -5,9 +5,7 @@ class StaticURLTests(TestCase):
     def test_about_url(self):
         response = Client().get('/about/')
         self.assertEqual(response.status_code, 200)
+        # Пожалуйста, пусть один такой будет ещё
+        # Я считаю, что он должен быть
         response = Client().get('/about/1/')
-        self.assertNotEqual(response.status_code, 200)
-        response = Client().get('/about/h/')
-        self.assertNotEqual(response.status_code, 200)
-        response = Client().get('/about1/')
         self.assertNotEqual(response.status_code, 200)
--- catalog/tests.py
+++ catalog/tests.py
@@ -2,8 +2,7 @@ from django.test import Client, TestCase
 
 
 class StaticURLTests(TestCase):
-    # Я не уверена, что это надо делать так, но я не поняла как по-другому
-    def test_positive_catalog_url(self):
+    def test_correct_catalog_url(self):
         tests = ['', '1/', '123/']
 
         for i in range(len(tests)):
@@ -11,7 +10,7 @@ class StaticURLTests(TestCase):
             with self.subTest(f'{tests[i][:-1]} is a positive integer', i=i):
                 self.assertEqual(response.status_code, 200)
 
-    def test_negative_catalog_url(self):
+    def test_wrong_catalog_url(self):
         tests = ['0', '-1', '0123', 'a', '1a', '1a2', 'a1',
                  'one', '_-', '_1', '1_3', '123_', '1/12']
 
