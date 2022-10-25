6bfc6502e2370e567ef1ccc7c26f617b0d929f36
ksusha <ktsykanova@yandex.ru>
2022-10-25 12:42:10 +0300

    update regex + make pep8 fixes

--- catalog/tests.py
+++ catalog/tests.py
@@ -4,7 +4,7 @@ from django.test import Client, TestCase
 class StaticURLTests(TestCase):
     # Я не уверена, что это надо делать так, но я не поняла как по-другому
     def test_positive_catalog_url(self):
-        tests = ['',  '1/',  '123/']
+        tests = ['', '1/', '123/']
 
         for i in range(len(tests)):
             response = Client().get('/catalog/' + tests[i])
@@ -13,7 +13,7 @@ class StaticURLTests(TestCase):
 
     def test_negative_catalog_url(self):
         tests = ['0', '-1', '0123', 'a', '1a', '1a2', 'a1',
-                 'one', '_-', '_1', '1_3', '123_']
+                 'one', '_-', '_1', '1_3', '123_', '1/12']
 
         for i in range(len(tests)):
             response = Client().get('/catalog/' + tests[i] + '/')
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path
 
 from . import views
+
 app_name = 'catalog'
 urlpatterns = [
     path('', views.item_list, name='item_list'),
-    re_path('(?P<pk>^[1-9]\\d*)/', views.item_detail, name='item_detail'),
+    re_path('(?P<pk>^[1-9]\\d*)/$', views.item_detail, name='item_detail'),
 ]
