e6f41bbb00438857da6cbe50ac692f2974f5be2c
ksusha <ktsykanova@yandex.ru>
2022-11-01 21:01:54 +0300

    update models again

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,3 @@
-from enum import unique
-from tabnanny import verbose
 from django.core.validators import (MaxValueValidator, MinValueValidator,
                                     RegexValidator)
 from django.db import models
